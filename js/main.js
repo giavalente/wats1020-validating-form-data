a7b2135ad37c2b5bfe349be844c49647b18d1128
valenteg <valenteg@seattleu.edu>
2017-03-18 04:18:52 -0400

    Styling for error and validation messages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 /* Custom JS goes here. */
-
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
-
-// Refer to the `index.html` file for the validation rules that must be enforced.
+                
+// Validate form rules
 $(document).on('ready', function(){
    $.validator.addMethod("valueNotEquals", function(value, element, arg){
   return arg != value;
@@ -61,7 +56,6 @@ $(document).on('ready', function(){
        "comments": {
          maxlength: 500, 
        },
-      
-    }
- });  
+     },
+ });   
 }); 
